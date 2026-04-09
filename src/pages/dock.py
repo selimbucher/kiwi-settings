91baf63f57b0541feaf0a7f9585a80397c05ca6d
Selim Bucher <selim.bucher@slimmer.ch>
2026-04-09 19:09:49 +0200

    feat: setting for dock icon size

--- src/pages/dock.py
+++ src/pages/dock.py
@@ -42,6 +42,16 @@ class DockPage(Adw.PreferencesPage):
         margin_row.connect("notify::value", self.on_margin_changed)
         appearance_group.add(margin_row)
 
+        icon_size_row = Adw.ActionRow(title="Icon Size")
+        icon_size_scale = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, 42, 64, 1)
+        icon_size_scale.set_value(get("dock_icon_size", 56))
+        icon_size_scale.set_draw_value(True)
+        icon_size_scale.set_hexpand(True)
+        icon_size_scale.set_valign(Gtk.Align.CENTER)
+        icon_size_scale.connect("value-changed", self.on_icon_size_changed)
+        icon_size_row.add_suffix(icon_size_scale)
+        appearance_group.add(icon_size_row)
+
         self.add(appearance_group)
 
     def on_mode_changed(self, row, _):
@@ -50,4 +60,8 @@ class DockPage(Adw.PreferencesPage):
 
     def on_margin_changed(self, row, _):
         set_conf("dock_margin", int(row.get_value()))
+        write_conf()
+    
+    def on_icon_size_changed(self, scale):
+        set_conf("dock_icon_size", int(scale.get_value()))
         write_conf()
